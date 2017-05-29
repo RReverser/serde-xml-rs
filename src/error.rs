6598b8be2b735cbdd4482101fcbe174640a7b8cb
Jan Jansen <jan.jansen@gdata-adan.de>
2017-05-29 13:42:57 +0200

    Some Normalization and switch to error_chain

--- src/error.rs
+++ src/error.rs
@@ -1,22 +1,36 @@
-use xml::reader;
-use std::fmt::{self, Display, Debug};
-use std::error::Error as StdError;
-use std::num;
+use std::fmt::Display;
+
 use serde::de::Error as SerdeError;
 
-pub enum Error {
-    ParseIntError(num::ParseIntError),
-    Syntax(reader::Error),
-    Custom(String)
-}
+error_chain! {
+    types {
+        Error,
+        ErrorKind,
+        ResultExt,
+        Result;
+    }
 
-pub type VResult<V> = Result<V, Error>;
+    foreign_links {
+        ParseIntError(::std::num::ParseIntError);
+        ParseFloatError(::std::num::ParseFloatError);
+        ParseBoolError(::std::str::ParseBoolError);
+        ParseError(::std::string::ParseError);
+        Syntax(::xml::reader::Error);
+    }
+
+    errors {
+        Custom(field: String) {
+            description("other error")
+            display("custom: '{}'", field)
+        }
+    }
+}
 
 macro_rules! expect {
     ($actual: expr, $($expected: pat)|+ => $if_ok: expr) => {
         match $actual {
             $($expected)|+ => $if_ok,
-            actual => Err($crate::Error::Custom(format!("Expected token {}, found {:?}", stringify!($($expected)|+), actual)))
+            actual => Err($crate::ErrorKind::Custom(format!("Expected token {}, found {:?}", stringify!($($expected)|+), actual)).into())
         }
     }
 }
@@ -41,46 +55,8 @@ macro_rules! debug_expect {
     }
 }
 
-impl Display for Error {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::ParseIntError(ref error) => Display::fmt(error, fmt),
-            Error::Syntax(ref error) => Display::fmt(error, fmt),
-            Error::Custom(ref display) => Display::fmt(display, fmt)
-        }
-    }
-}
-
-impl Debug for Error {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::ParseIntError(ref error) => Display::fmt(error, fmt),
-            Error::Syntax(ref error) => Debug::fmt(error, fmt),
-            Error::Custom(ref display) => Display::fmt(display, fmt)
-        }
-    }
-}
-
-impl StdError for Error {
-    fn description(&self) -> &str {
-        match *self {
-            Error::ParseIntError(ref error) => error.description(),
-            Error::Syntax(ref error) => error.description(),
-            Error::Custom(_) => "other error"
-        }
-    }
-
-    fn cause(&self) -> Option<&StdError> {
-        match *self {
-            Error::ParseIntError(ref error) => Some(error),
-            Error::Syntax(ref error) => Some(error),
-            _ => None
-        }
-    }
-}
-
 impl SerdeError for Error {
     fn custom<T: Display>(msg: T) -> Self {
-        Error::Custom(msg.to_string())
+        ErrorKind::Custom(msg.to_string()).into()
     }
 }
--- src/var.rs
+++ src/var.rs
@@ -2,19 +2,18 @@ use std::io::Read;
 use xml::name::OwnedName;
 use xml::reader::XmlEvent;
 use {Deserializer, Error};
-use serde::de::{self, DeserializeSeed, Deserializer as SerdeDeserializer, Visitor, Error as SerdeError};
+use serde::de::{self, DeserializeSeed, Deserializer as SerdeDeserializer, Visitor,
+                Error as SerdeError};
 use serde::de::IntoDeserializer;
-use VResult;
+use error::Result;
 
 pub struct EnumAccess<'a, R: 'a + Read> {
-    de: &'a mut Deserializer<R>
+    de: &'a mut Deserializer<R>,
 }
 
 impl<'a, R: 'a + Read> EnumAccess<'a, R> {
     pub fn new(de: &'a mut Deserializer<R>) -> Self {
-        EnumAccess {
-            de: de
-        }
+        EnumAccess { de: de }
     }
 }
 
@@ -22,31 +21,31 @@ impl<'de, 'a, R: 'a + Read> de::EnumAccess<'de> for EnumAccess<'a, R> {
     type Error = Error;
     type Variant = VariantAccess<'a, R>;
 
-    fn variant_seed<V: DeserializeSeed<'de>>(self, seed: V) -> Result<(V::Value, VariantAccess<'a, R>), Error> {
-        let name = expect!(self.de.peek()?, &XmlEvent::Characters(ref name) | &XmlEvent::StartElement { name: OwnedName { local_name: ref name, .. }, .. } => {
+    fn variant_seed<V: DeserializeSeed<'de>>(self,
+                                             seed: V)
+                                             -> Result<(V::Value, VariantAccess<'a, R>)> {
+        let name: Result<V::Value> = expect!(self.de.peek()?, &XmlEvent::Characters(ref name) | &XmlEvent::StartElement { name: OwnedName { local_name: ref name, .. }, .. } => {
             seed.deserialize(name.as_str().into_deserializer())
-        })?;
+        });
         self.de.set_map_value();
-        Ok((name, VariantAccess::new(self.de)))
+        Ok((name?, VariantAccess::new(self.de)))
     }
 }
 
 pub struct VariantAccess<'a, R: 'a + Read> {
-    de: &'a mut Deserializer<R>
+    de: &'a mut Deserializer<R>,
 }
 
 impl<'a, R: 'a + Read> VariantAccess<'a, R> {
     pub fn new(de: &'a mut Deserializer<R>) -> Self {
-        VariantAccess {
-            de: de
-        }
+        VariantAccess { de: de }
     }
 }
 
 impl<'de, 'a, R: 'a + Read> de::VariantAccess<'de> for VariantAccess<'a, R> {
     type Error = Error;
 
-    fn unit_variant(self) -> Result<(), Error> {
+    fn unit_variant(self) -> Result<()> {
         self.de.unset_map_value();
         match self.de.next()? {
             XmlEvent::StartElement { name, attributes, .. } => {
@@ -55,23 +54,24 @@ impl<'de, 'a, R: 'a + Read> de::VariantAccess<'de> for VariantAccess<'a, R> {
                 } else {
                     Err(Error::invalid_length(attributes.len(), &"0"))
                 }
-            },
-            XmlEvent::Characters(_) => {
-                Ok(())
             }
-            _ => unreachable!()
+            XmlEvent::Characters(_) => Ok(()),
+            _ => unreachable!(),
         }
     }
 
-    fn newtype_variant_seed<T: DeserializeSeed<'de>>(self, seed: T) -> Result<T::Value, Error> {
+    fn newtype_variant_seed<T: DeserializeSeed<'de>>(self, seed: T) -> Result<T::Value> {
         seed.deserialize(&mut *self.de)
     }
 
-    fn tuple_variant<V: Visitor<'de>>(self, len: usize, visitor: V) -> VResult<V::Value> {
+    fn tuple_variant<V: Visitor<'de>>(self, len: usize, visitor: V) -> Result<V::Value> {
         self.de.deserialize_tuple(len, visitor)
     }
 
-    fn struct_variant<V: Visitor<'de>>(self, _fields: &'static [&'static str], visitor: V) -> VResult<V::Value> {
+    fn struct_variant<V: Visitor<'de>>(self,
+                                       _fields: &'static [&'static str],
+                                       visitor: V)
+                                       -> Result<V::Value> {
         self.de.deserialize_map(visitor)
     }
 }
